fc4445e7f16965f62bc73d1e6b8b557d8fc826f6
nitinkumar2003 <nitinkumarja2003@gmail.com>
2024-08-06 22:30:14 +0530

    changes

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ require('./db/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const bodyParser= require('body-parser');
-const fileupload = require('express-fileupload'); 
+const bodyParser = require('body-parser');
+const fileupload = require('express-fileupload');
 const userRoute = require('./routes/userRoute')
 const otpRoute = require('./routes/otpRoute')
 const propertyRoute = require('./routes/propertyRoute')
@@ -17,7 +17,7 @@ const errorHandler = require('./middleware/errorHandling')
 app.use(cors())
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(fileupload({useTempFiles: true}))
+app.use(fileupload({ useTempFiles: true }))
 // app.use(errorHandler)
 
 
@@ -36,13 +36,13 @@ app.use('/address', address)
 app.use('/image', image)
 
 app.get('/', (req, res) => {
-    res.json('server start')
+    res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
     console.log('server start ')
 })
 
 
-const port=process.env.DB_PORT || 4001
-var server = app.listen(port,"::", function () {
+const port = process.env.DB_PORT || 4001
+var server = app.listen(port, "::", function () {
     var host = server.address().address
     var port = server.address().port
 
